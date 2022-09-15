f8673555f35b64747d4e19fdab89cd8b045324ea
Silas <silas.oliveira8@gmail.com>
2022-09-15 16:56:11 -0300

    feat: req 2

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const IVehicleSchema = z.object({
+  model: z
+    .string()
+    .min(3),
+  year: z
+    .number()
+    .int()
+    .positive()
+    .gte(1900)
+    .lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof IVehicleSchema>;
