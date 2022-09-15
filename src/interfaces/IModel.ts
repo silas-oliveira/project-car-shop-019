c115cad411d6c77647a2d62d3d77e64faf79fb5b
Silas <silas.oliveira8@gmail.com>
2022-09-15 16:21:43 -0300

    feat: req 1

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,7 @@
+export interface IModel<T> {
+  create(body: T): Promise<T>;
+  read(): Promise<T[]>;
+  readOne(id: string): Promise<T | null>;
+  update(id: string, body: T): Promise<T | null>;
+  delete(id: string): Promise<T | null>;
+}
